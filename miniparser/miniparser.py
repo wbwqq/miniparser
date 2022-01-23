6eceaad9c2ea2283226e4ae55d99ad3278ccb2bd
wbwqq <74458558+wbwqq@users.noreply.github.com>
2022-01-23 19:14:35 +0100

    handling of multiword commands

--- miniparser/miniparser.py
+++ miniparser/miniparser.py
@@ -1,48 +1,45 @@
 import sys
 
-commands = {}
+commands = []
 
 def add_command(cmd, action, nargs=1, help=''):
-    commands[cmd]={'action': action, 'nargs': nargs, 'help': help}
+    commands.append({
+        'cmd': cmd,
+        'action': action, 
+        'nargs': nargs, 
+        'help': help, 
+        'len_cmd': len(cmd.split())
+    })
 
 def parser(args=sys.argv):
-    if len(args) < 2 or args[1] not in commands.keys():
-        arg = ''
-    else:
-        arg = args[1]
-    try:
-        for k,v in commands.items():
-            if k == arg and k != '':
-                if v['nargs'] == -1:
-                    args_rest = args[2:]
-                else:
-                    args_rest = args[2:2+v['nargs']]
-                return commands[arg]['action'](*args_rest)
-        # if no command with arguments:
-        if v['nargs'] == -1:
-            args_rest = args[1:]
-        else:
-            args_rest = args[1:1+v['nargs']]
-        return commands[arg]['action'](*args_rest)
-    except:
-        print("Argument error: non existing command or invalid command argument. Type '--help' for usage information.")
+    commands_sorted = sorted(commands, key=lambda x:x['len_cmd'], reverse=True)
+    for command in commands_sorted:
+        args_split = command['len_cmd']+1
+        cmd_cli = ' '.join(args[1:args_split])
+        args_cli = args[args_split:]
+        args_pass = args_cli[:command['nargs']]
+        if command['nargs'] == -1:
+            args_pass = args_cli
+        if cmd_cli == command['cmd']:
+            return command['action'](*args_pass)
 
 def print_help():
     file_name = sys.argv[0]
-    help_title = '{} help'.format(file_name)
+    help_title = '{0}{1} help{0}'.format(' '*4, file_name)
     help_border = '-'*len(help_title)
     print(help_border)
     print(help_title)
     print(help_border)
     no_arg = '(no cmd)'
-    max_command_len = max([len(c) for c in list(commands.keys())+[no_arg]])
+    max_command_len = max([len(c['cmd']) for c in commands])
     left_len = max_command_len + 4
-    for k,v in commands.items():
-        if k == '':
-            help_str = v['help']
+    commands_sorted = sorted(commands, key=lambda x:x['cmd'])
+    for command in commands_sorted:
+        if command['cmd'] == '':
+            help_str = command['help']
             print(no_arg.ljust(left_len, ' ') + help_str)
         else:
-            help_str = v['help']
-            print(k.ljust(left_len, ' ') + help_str)
+            help_str = command['help']
+            print(command['cmd'].ljust(left_len, ' ') + help_str)
 
 add_command('--help', print_help, nargs=0, help='print help')
